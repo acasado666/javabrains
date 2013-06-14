c087ef23e6793ce0af56c459fb17e786b9359b0d
koushikkothagal <koush.s@gmail.com>
2013-06-14 16:25:13 -0700

    Added JS for course listing

--- javascripts/springCore.js
+++ javascripts/springCore.js
@@ -4,3 +4,29 @@ var koushikSpringCoreCourse = {"bindings": [
         {"name": "Understanding Spring Bean Factory", "url": "http://javabrains.koushik.org/2011/08/spring-tutorial-03-understanding-spring.html"}
     ]
 };
+$(document).ready(function() {
+   var loc = window.location;
+   var listContent = "<table style='width:100%'>";
+   for (i in koushikSpringCoreCourse.bindings) {
+     var courseLinkCSS = "courseLink";
+     if($.trim(loc) == koushikSpringCoreCourse.bindings[i].url) {
+        courseLinkCSS = "currentPageLink";
+     }
+     var j = parseInt(i) + 1;
+     listContent = listContent + "<tr><td class='" + courseLinkCSS + "'>";
+     listContent = listContent + "<a style='display:block;' href='" + koushikSpringCoreCourse.bindings[i].url + "'> " + j + ". " + koushikSpringCoreCourse.bindings[i].name + "</a>";
+     listContent = listContent + "</td></tr>";
+   }
+   listContent = listContent + "</table>";
+   $('#slidingDrawer').append(listContent);
+   $('#viewCourse').click(function() {
+     
+     if ($('#slidingDrawer').is(":hidden")) {
+       $('#slidingDrawer').slideDown('slow');
+     } else {
+       $('#slidingDrawer').hide();
+     }
+
+   });
+
+});
