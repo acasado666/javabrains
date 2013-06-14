0f4f3145a88429b40a2c7066a09ea415603d35b7
koushikkothagal <koush.s@gmail.com>
2013-06-14 11:27:25 -0700

    Added spring core js ver 1

---
+++ javascripts/springCore.js
@@ -0,0 +1,6 @@
+var koushikSpringCoreCourse = {"bindings": [
+        {"name": "Understanding Dependency Injection", "url": "http://javabrains.koushik.org/2011/08/spring-tutorial-01-understanding.html"},
+        {"name": "Setting Up", "url": "http://javabrains.koushik.org/2011/08/spring-tutorial-02-setting-up.html"},
+        {"name": "Understanding Spring Bean Factory", "url": "http://javabrains.koushik.org/2011/08/spring-tutorial-03-understanding-spring.html"}
+    ]
+};
